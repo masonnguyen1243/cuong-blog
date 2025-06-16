85bd9c8ada6e23fca1fdffea2656d6c56c415b5e
cuongnguyenba1243 <2121050937@student.humg.edu.vn>
2025-06-16 16:57:37 +0700

    01: Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "~/components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <h1 className="text-red-500">Hello</h1>
+    </div>
+  );
+};
+export default App;
