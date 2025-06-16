48e9cdb16da0a8a78ca86a1ed4a34afa3dc555e6
cuongnguyenba1243 <2121050937@student.humg.edu.vn>
2025-06-16 19:23:20 +0700

    01: Setup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
-import Header from "~/components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "~/pages/Home";
+import About from "~/pages/About";
+import SignIn from "~/pages/SignIn";
+import SignUp from "~/pages/SignUp";
+import Dashboard from "~/pages/Dashboard";
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <h1 className="text-red-500">Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
