10fb9112ae03334bd653d43e24e476d434922d9f
cuongnguyenba1243 <2121050937@student.humg.edu.vn>
2025-06-21 23:11:30 +0700

    22: Profile page layout

--- frontend/src/components/Dashboard/DashboardProfile.jsx
+++ frontend/src/components/Dashboard/DashboardProfile.jsx
@@ -1,4 +1,42 @@
+import { Button, TextInput } from "flowbite-react";
+import { useSelector } from "react-redux";
+
 const DashboardProfile = () => {
-  return <div>DashboardProfile</div>;
+  const { user } = useSelector((state) => state.auth);
+
+  return (
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <div className="w-32 h-32 cursor-pointer shadow-md overflow-hidden rounded-full self-center">
+          <img
+            src={user?.data?.rest?.avatar}
+            alt="avatar"
+            className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
+          />
+        </div>
+
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={user?.data?.rest?.username}
+        />
+        <TextInput
+          type="email"
+          id="email"
+          placeholder="email"
+          defaultValue={user?.data?.rest?.email}
+        />
+        <TextInput type="password" id="password" placeholder="password" />
+        <Button
+          type="submit"
+          className="bg-gradient-to-r from-purple-400 to-blue-400 cursor-pointer"
+        >
+          Update
+        </Button>
+      </form>
+    </div>
+  );
 };
 export default DashboardProfile;
