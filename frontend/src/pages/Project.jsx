ce98810b41cdad3fcc76f422359de4ff610ff08c
cuongnguyenba1243 <2121050937@student.humg.edu.vn>
2025-07-03 15:42:09 +0700

    50: Project page

--- frontend/src/pages/Project.jsx
+++ frontend/src/pages/Project.jsx
@@ -1,4 +1,14 @@
+import CallToAction from "~/components/Post/CallToAction";
+
 const Project = () => {
-  return <div>Project</div>;
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Project</h1>
+      <p className="text-lg text-gray-500">
+        Build fun and engaging projects while learning HTML, CSS, JavaScript
+      </p>
+      <CallToAction />
+    </div>
+  );
 };
 export default Project;
