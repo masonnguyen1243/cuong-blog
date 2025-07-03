3837cdad9ec00847fabd7dfa15a465d7eeadb6f4
cuongnguyenba1243 <2121050937@student.humg.edu.vn>
2025-07-03 15:39:29 +0700

    49: About page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,4 +1,35 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Cuong's Blog
+          </h1>
+          <div className="text-lg text-gray-500 flex flex-col gap-6">
+            <p>
+              Cuong's Blog is a blog that I created to share my througts and
+              ideals with the word. I'm a software engineer and I love to write
+              about my expreriences and things that i have learned. I hope you
+              enjoy reading my blog
+            </p>
+            <p>
+              On this blog, you'll find weekly articles and tutorials covering
+              topics like web development, software engineering, and programming
+              languages. Sahand is constantly learning and exploring new
+              technologies, so don't forget to check back often for the latest
+              updates!
+            </p>
+            <p>
+              We encourage you to leave comments on posts and engage with other
+              readers. You can "like" others' comments and reply to them. We
+              believe that a community of curious learners can support one
+              another in growing together.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default About;
