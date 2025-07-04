9d1abd821ca26c8cd665a3c4f305cc6c35d7ae60
cuongnguyenba1243 <2121050937@student.humg.edu.vn>
2025-07-04 13:49:28 +0700

    update

--- frontend/src/pages/Project.jsx
+++ frontend/src/pages/Project.jsx
@@ -5,7 +5,7 @@ const Project = () => {
     <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
       <h1 className="text-3xl font-semibold">Project</h1>
       <p className="text-lg text-gray-500">
-        Build fun and engaging projects while learning HTML, CSS, JavaScript
+        Build fun and engaging projects while learning HTML, CSS, JavaScript.
       </p>
       <CallToAction />
     </div>
